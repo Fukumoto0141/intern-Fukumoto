8d020a3a66a779c5884d0617a402637ccdce90fe
わけわかめ <>
2023-03-13 17:35:50 +0900

    一覧画面に書籍一覧を表示

--- intern-book-app/src/app/pages/list/list.component.ts
+++ intern-book-app/src/app/pages/list/list.component.ts
@@ -1,4 +1,7 @@
+import { DeclarationListEmitMode } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { Book } from '../../types/book';
+
 
 @Component({
   selector: 'app-list',
@@ -7,4 +10,16 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
+  bookList: Book[] = [
+    {
+      name: 'キタミ式イラストIT塾 応用情報技術者 令和05年',
+      detail: 'なんかすごい',
+      evaluation: 2
+    },
+    {
+      name: '漫画',
+      detail: '面白い',
+      evaluation: 100
+    }
+  ];
 }
